62e83a73a7e2b92f5adb55177fa90c9ae677ff1b
AhrorShoH Umrzoqov <90472303+AhrorShoH@users.noreply.github.com>
2022-07-10 10:38:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import './App.css';
+import Blog from './blog/Blog';
+import { Route } from 'react-router';
+import About from './about/About';
+import Portfolio from './portfolio/Portfolio';
+import {Routes} from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-     <Navbar /> 
-      <Main />
-            
+      <Navbar/>
+      <Routes>
+        <Route index path='/' element={<Main/>} />
+        <Route path='/blog' element={<Blog/>} />
+        <Route path='/portfolio' element={<Portfolio/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
     </div>
   );
 }
