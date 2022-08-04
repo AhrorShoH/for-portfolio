961180e1b1c5630e749f1473cf1b32a2b3ebf168
Ahrorbek Umrzoqov <90472303+ahrorbek-umrzoqov@users.noreply.github.com>
2022-08-04 22:24:12 +0500

    Update Blog.js

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import "./blog.css"
-import Navbar from '../components/Navbar';
+import React from "react";
+import "./blog.css";
+import Navbar from "../components/Navbar";
 
 class Blog extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar/>
-                <div className='blog'>
-                    <h1 id='text'>404 Not Found</h1>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="blog">
+          <h3 id="text">
+            <h3 id="caption">Programming.</h3><br/> Programming is the process of creating, testing, and
+            modifying programs for computers and other microprocessor-based
+            electronic machines. People who create software are called
+            Developers. There are different fields of programming such as Mobile
+            Programming, Front End Programming, Back End Programming, Artificial
+            Intelligence etc.
+          </h3>
+        </div>
+      </div>
+    );
+  }
 }
-
-
 
 export default Blog;
